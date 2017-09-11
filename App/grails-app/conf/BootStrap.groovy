5a375a8547952e37facbda61a3024fbfd8353c23
Basis <Basis@Sineti>
2017-09-11 12:03:59 -0500

    auto-commit to master branch

--- App/grails-app/conf/BootStrap.groovy
+++ App/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import app.Ship
+
 class BootStrap {
 
     def init = { servletContext ->
+		new Ship(nombre : "Lucero",numeroEmpleados : 20,recamaras : 10, numeroTuristas : 25).save()
+		new Ship(nombre : "Itzel",numeroEmpleados : 30,recamaras : 20, numeroTuristas : 35).save()
+		new Ship(nombre : "Fernanda",numeroEmpleados : 10,recamaras : 50, numeroTuristas : 15).save()
     }
     def destroy = {
     }
